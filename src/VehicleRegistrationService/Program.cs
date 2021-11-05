f5a712510f20276b4fde459a97b2a2170ad91e0e
mikelapierre <mike.lapierre@live.com>
2021-11-05 17:10:13 -0400

    Trigger CI

--- src/VehicleRegistrationService/Program.cs
+++ src/VehicleRegistrationService/Program.cs
@@ -15,6 +15,7 @@ namespace VehicleRegistrationService
         {
             CreateHostBuilder(args).Build().Run();
         }
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
