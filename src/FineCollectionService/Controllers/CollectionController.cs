48ced6c77be154541b7789e179bc99d31de478ef
Mike Lapierre <mike.lapierre@live.com>
2021-11-08 11:21:04 -0500

    don't use k8s secrets

--- src/FineCollectionService/Controllers/CollectionController.cs
+++ src/FineCollectionService/Controllers/CollectionController.cs
@@ -32,20 +32,20 @@ namespace FineCollectionService.Controllers
             // set finecalculator component license-key
             if (_fineCalculatorLicenseKey == null)
             {
-                bool runningInK8s = Convert.ToBoolean(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") ?? "false");
-                var metadata = new Dictionary<string, string> { { "namespace", "dapr-trafficcontrol" } };
-                if (runningInK8s)
-                {
-                    var k8sSecrets = daprClient.GetSecretAsync(
-                        "kubernetes", "trafficcontrol-secrets", metadata).Result;
-                    _fineCalculatorLicenseKey = k8sSecrets["finecalculator.licensekey"];
-                }
-                else
-                {
+                // bool runningInK8s = Convert.ToBoolean(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") ?? "false");
+                // var metadata = new Dictionary<string, string> { { "namespace", "dapr-trafficcontrol" } };
+                // if (runningInK8s)
+                // {
+                //     var k8sSecrets = daprClient.GetSecretAsync(
+                //         "kubernetes", "trafficcontrol-secrets", metadata).Result;
+                //     _fineCalculatorLicenseKey = k8sSecrets["finecalculator.licensekey"];
+                // }
+                // else
+                // {
                     var secrets = daprClient.GetSecretAsync(
                         "trafficcontrol-secrets", "finecalculator-licensekey").Result;
                     _fineCalculatorLicenseKey = secrets["finecalculator-licensekey"];
-                }
+                // }
             }
         }
 
