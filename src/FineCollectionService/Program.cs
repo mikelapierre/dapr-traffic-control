20c33b315fcbe1f00d42481dcff801c3d2646100
Mike Lapierre <mike.lapierre@live.com>
2021-11-08 11:48:05 -0500

    ci bump

--- src/FineCollectionService/Program.cs
+++ src/FineCollectionService/Program.cs
@@ -16,6 +16,7 @@ namespace FineCollectionService
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
