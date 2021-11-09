bcdc87f55dae3d5d55bc48cd760580f0eb0580c3
Mike Lapierre <mike.lapierre@live.com>
2021-11-09 10:15:13 -0500

    CI bump

--- src/TrafficControlService/Startup.cs
+++ src/TrafficControlService/Startup.cs
@@ -39,6 +39,7 @@ namespace TrafficControlService
             {
                 options.Actors.RegisterActor<VehicleActor>();
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
